a7cf18e1b275c08e9fcf31132236453a5088a56a
Kayhan <kdehghan@sfu.ca>
2017-11-09 12:40:23 -0800

    Modify academics page and general styling.

--- app/src/main/resources/static/js/main.js
+++ app/src/main/resources/static/js/main.js
@@ -1,15 +1,3 @@
-function xhr_get(url) {
-	return $.ajax( {
-		url: url,
-		type: 'get',
-//		beforeSend: showSpinner
-	}).always(function(data) {
-		// stop spinner
-	}).fail(function(message) {
-		// show error message
-	});
-}
-
 $(document).ready(function() {
-
+	
 });
