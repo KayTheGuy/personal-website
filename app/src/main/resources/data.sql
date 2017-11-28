fdcfc5618e2fe1c555bf2d7ad8840887ce984287
Kayhan <kdehghan@sfu.ca>
2017-11-28 15:42:49 -0800

    Add modal preview for album images

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -7,14 +7,18 @@ INSERT INTO IMAGE VALUES('lion-gate', 'jpg', '/files/album/lion-gate.jpg', '1087
 INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
 INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
 INSERT INTO IMAGE VALUES('lion-gate', 'jpg', '/files/album/lion-gate.jpg', '1087641');
-
+INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
 INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
 INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
 INSERT INTO IMAGE VALUES('lion-gate', 'jpg', '/files/album/lion-gate.jpg', '1087641');
-
+INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
+INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
 INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
 INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
 INSERT INTO IMAGE VALUES('lion-gate', 'jpg', '/files/album/lion-gate.jpg', '1087641');
-
+INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
+INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
+INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
+INSERT INTO IMAGE VALUES('lion-gate', 'jpg', '/files/album/lion-gate.jpg', '1087641');
 INSERT INTO IMAGE VALUES('downtown', 'jpg', '/files/album/downtown.jpg', '155796');
 INSERT INTO IMAGE VALUES('buntzen', 'jpg', '/files/album/buntzen.jpg', '77861');
--- app/src/main/resources/static/js/imageAlbum.js
+++ app/src/main/resources/static/js/imageAlbum.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+	// get album images
 	$.ajax({
 		url : '/api/album',
 		type : 'GET',
@@ -9,18 +9,30 @@ $(document).ready(function() {
 	}).done(function(imgList) {
 		var currentPic;
 		var currentPicDiv;
+		var currentSpaceDiv;
+		var currentMiddleDiv;
 		var currentRow;
 		var currentRowIndx = 0;
 		for(var i = 0; i < imgList.length; i++) {
 			var album = $('#img-album-content');
 			if(i % 3 === 0) {
 				currentRow = $("<div>", {id: "row" + currentRowIndx++, "class": "mdl-grid"});
+				currentSpaceDiv = $("<div>", {"class": "mdl-cell mdl-cell--2-col"});
+				currentRow.append(currentSpaceDiv);
 				album.append(currentRow);
 			} 
-			currentPicDiv = $("<div>", {id: "img" + i, "class": "mdl-cell mdl-cell--4-col"});
-			currentPic = $("<img>", {src: imgList[i].path, "class": "album-img-cell", alt: imgList[i].name, height: "200", width: "300"});
+			currentPicDiv = $("<div>", {id: "img" + i, "class": "album-image-div mdl-cell mdl-cell--3-col"});
+			currentPic = $("<img>", {src: imgList[i].path, "class": "album-image", alt: imgList[i].name, height: "150", width: "225"});
+			currentMiddleDiv = "<div class=\"album-img-middle\"><div class=\"album-img-text\"><i class=\"image-map material-icons\">place</i>" 
+								+  imgList[i].name
+								+ "</div></div>";
 			currentPicDiv.append(currentPic);
+			currentPicDiv.append(currentMiddleDiv);
 			currentRow.append(currentPicDiv);
+			if(i % 3 === 2) {
+				currentSpaceDiv = $("<div>", {"class": "mdl-cell mdl-cell--1-col"});
+				currentRow.append(currentSpaceDiv);
+			}
 		}
 	}).always(function() {
 		// always
@@ -28,4 +40,24 @@ $(document).ready(function() {
 		showError("Failed to send message. Please Try again." + message.responseText);
 	});
 	
+	// event handler for dynamically added elements
+	var modal = document.getElementById('contactSentMsg');
+	$(document).on ("click", ".album-image", function () {
+		// show message
+		var imageElement = $('#image-preview');
+		imageElement.attr('src', this.src);
+		modal.style.visibility = "visible";
+		
+    });
+	
+	// close modal handlers
+	$('#modalClose').on('click', function() {
+	    modal.style.visibility = "hidden";
+	});
+	
+	$(document).on('click touchstart', function(event) {
+	    if (event.target == modal) {
+	        modal.style.visibility = "hidden";
+	    }
+	});
 });
