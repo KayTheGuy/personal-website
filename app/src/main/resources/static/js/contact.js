1f5e9ffdd2569211d7cbc62eb3d4746732f47143
Kayhan <kdehghan@sfu.ca>
2018-02-03 05:04:01 -0800

    Modify styling

--- app/src/main/resources/static/js/contact.js
+++ app/src/main/resources/static/js/contact.js
@@ -5,7 +5,7 @@ function checkEmailInput(email) {
 	var result = re.test(email);
 	if(!result) {
 		formErrMsg = 'You have entered an invalid email address.';
-		$('#contact-form-email').css({ borderBottom: '1px solid #00b5ec'});
+		$('#contact-form-email').css({ borderBottom: '1px solid #e61344'});
 		return false;
 	}
 	$('#contact-form-email').css({ borderBottom: '1px solid rgba(0,0,0,.12)'});
@@ -15,7 +15,7 @@ function checkEmailInput(email) {
 function checkEmptyInput(input, field) {
 	if(!input) { // empty field
 		formErrMsg = 'Please provide a valid ' + field;
-		$('#contact-form-' + field).css({ borderBottom: '1px solid #00b5ec'});
+		$('#contact-form-' + field).css({ borderBottom: '1px solid #e61344'});
 		return false;
 	}
 	$('#contact-form-' + field).css({ borderBottom: '1px solid rgba(0,0,0,.12)'});
