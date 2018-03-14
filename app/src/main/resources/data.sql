d89a27d2e038c36349e4cc3263faa8fd9c4068ed
Kayhan <kdehghan@sfu.ca>
2018-03-13 22:46:29 -0700

    Fix contact form page and home button alignment

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -2,8 +2,8 @@ USE personal_website;
 
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -14,13 +14,10 @@ INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jp
 
 
 
-
-
-
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -29,8 +26,8 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -39,8 +36,8 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -49,8 +46,8 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -59,8 +56,8 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -69,8 +66,8 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -79,8 +76,8 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
@@ -89,12 +86,11 @@ INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg'
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
 INSERT INTO IMAGE VALUES('Brown Wall', 'jpg', '/files/album/Brown-Wall.jpg', 49.2772059, -123.0829845, '2017-03-04');
 INSERT INTO IMAGE VALUES('Diez Vista View Point', 'jpg', '/files/album/Buntzen-View.jpg', 49.3467, -122.8790, '2017-04-23');
-INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.3467, -122.8790, '2017-03-10');
-INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Couple-Walking.jpg', 49.2833782, -123.103902, '2017-04-21');
+INSERT INTO IMAGE VALUES('Cool Couples', 'jpg', '/files/album/Couple-Walking.jpg', 49.2772059, -123.08298, '2017-03-10');
+INSERT INTO IMAGE VALUES('Hotel Europe', 'jpg', '/files/album/Hotel-Europe.jpg', 49.2833782, -123.103902, '2017-04-21');
 INSERT INTO IMAGE VALUES('GT4', 'jpg', '/files/album/GT4.jpg', 49.2811806, -123.1009695, '2017-03-15');
 INSERT INTO IMAGE VALUES('Harbour Center', 'jpg', '/files/album/Harbour-Center.jpg', 49.2838256, -123.1106928, '2017-04-23');
 INSERT INTO IMAGE VALUES('Coal Harbour', 'jpg', '/files/album/Stanley-Harbour.jpg', 49.2975529, -123.1263058, '2017-03-04');
 INSERT INTO IMAGE VALUES('Cool Windows', 'jpg', '/files/album/Windows-Branches.jpg', 49.2839641, -123.1208498, '2017-03-04');
 INSERT INTO IMAGE VALUES('Other Cool Windows', 'jpg', '/files/album/Windows.jpg', 49.2839641, -123.1208498, '2017-03-04');
 INSERT INTO IMAGE VALUES('Reflection', 'jpg', '/files/album/Yellow-Reflection.jpg', 49.2822651, -123.0813408, '2017-03-10');
-
