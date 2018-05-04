4d0aa57bbd8d56b8d6f1d9457a7515e9f5ef2567
Kayhan <kdehghan@sfu.ca>
2018-05-03 21:03:05 -0700

    Modify text typer in index page

--- app/src/main/resources/static/js/index.js
+++ app/src/main/resources/static/js/index.js
@@ -1,18 +1,18 @@
-var myLanguage = [ "Java", "JavaScript", "Python", "SQL", "CSS", "HTML" ];
+var myLanguage = [ "Java", "Python", "JavaScript", "SQL", "CSS", "HTML" ];
+var myTools = [ "Spring", "SQL Server", "MySQL", "MongoDB", "Git" ];
 var lanStyle = "{ font-family: \"Poiret One\"; font-size: 140%; }";
 var introStyle = "{ font-weight: 400; letter-spacing: 0.1em; }";
 
-var TextTyper = function(el, id, dataList, period, loop, style, delta1, delta2, fallback) {
+var TextTyper = function(el, id, dataList, period, style, delta1, delta2, fallback) {
 	this.el = el;
 	this.id= id;
 	this.dataList = dataList;
-	this.loop = loop;
 	this.period = parseInt(period, 10) || 2000;
 	this.style = style;
 	this.delta1 = delta1;
 	this.delta2 = delta2;
 	this.fallback = fallback;
-	this.loopNum = 0;
+	this.count = 0;
 	this.txt = '';
 	this.typeIt();
 	if(style) this.applyCSS(style);
@@ -27,46 +27,54 @@ TextTyper.prototype.applyCSS = function() {
 }
 
 TextTyper.prototype.typeIt = function() {
-	var i = this.loopNum % this.dataList.length;
-	var fullTxt = this.dataList[i];
-	
-	if (this.isDeleting) {
-		this.txt = fullTxt.substring(0, this.txt.length - 1);
-	} else {
-		this.txt = fullTxt.substring(0, this.txt.length + 1);
-	}
-
-	this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-	var that = this;
-	var delta = this.delta1 - Math.random() * 100;
-	
-	if (this.isDeleting) { delta /= 1.5; }
-	
-	if (!this.isDeleting && this.txt === fullTxt) {
-		delta = this.period;
-		this.isDeleting = true;
-		if(!this.loop) {
-			if(this.fallback) this.fallback();
+	if(this.count < this.dataList.length) {
+		var fullTxt = this.dataList[this.count];
+		
+		if (this.isDeleting) {
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
 		}
-	} else if (this.isDeleting && this.txt === '') {
-		this.isDeleting = false;
-		this.loopNum++;
-		delta = this.delta2;
+		
+		this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+		var that = this;
+		var delta = this.delta1 - Math.random() * 100;
+		
+		if (this.isDeleting) { delta /= 1.5; }
+		
+		if (!this.isDeleting && this.txt === fullTxt) {
+			delta = this.period;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === '') {
+			this.isDeleting = false;
+			this.count++;
+			delta = this.delta2;
+		}
+		
+		setTimeout(function() {
+			that.typeIt();
+		}, delta);
+	} else {
+		if(this.fallback) this.fallback();
 	}
-	
-	setTimeout(function() {
-		that.typeIt();
-	}, delta);
 };
 
-var typeForClass = function(id, data, period, loop, style, delta1, delta2, showIntroButton) {
+var typeForClass = function(id, data, period, style, delta1, delta2, fallback) {
 	var element = document.getElementById(id);
-	new TextTyper(element, id, data, period, loop, style, delta1, delta2, showIntroButton);
+	new TextTyper(element, id, data, period, style, delta1, delta2, fallback);
+};
+
+var toggleLoop = function() {
+	var skillTitleEl = document.getElementById('skills-title');
+	if(skillTitleEl.innerHTML === 'Languages: ') {
+		skillTitleEl.innerHTML = 'Tools: '
+		typeForClass('skills-value', myTools, 1000, lanStyle, 140, 500, null);
+	}
 };
 
 $(window).on('load', function() {
 	var coursesVisible = false;
-	typeForClass('languages', myLanguage, 1000, true, lanStyle, 200, 400, null);
+	typeForClass('skills-value', myLanguage, 1000, lanStyle, 140, 500, toggleLoop);
 	// COURSES
 	$('#show-courses-button').click(function() {
 		if (coursesVisible) {
